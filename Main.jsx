8ecc650ccc5b3cd4a03c880e205be78094cbdd7a
shubhan912 <2023.shubhan.sawant@ves.ac.in>
2025-07-21 17:58:07 +0530

    Initial commit

---
+++ Main.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import {getRecipeFromMistral } from "./ai"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState(
+        []
+    )
+    const [recipe, setRecipe] = React.useState("")
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+        event.target.reset(); // Optional: clear input after adding
+    }
+
+    return (
+        <main>
+            <div className="description">
+            <p>Chef Claude helps you generate delicious recipes using AI — just tell it what you have or what you’re craving, and it’ll whip up a recipe instantly.</p>
+            <p >
+                <strong>
+                    <em>
+                        Chef Claude is a work in progress. It's not perfect, but it's a fun way to experiment with AI and generate recipes.
+                    </em>
+                </strong>
+            </p>
+            </div>
+            <form onSubmit={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
+}
